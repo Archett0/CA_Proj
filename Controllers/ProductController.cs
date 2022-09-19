bf75e353ab8b4f2690ae60236270172e34410382
Yuanchi Liu <mickeylyc@outlook.com>
2022-09-19 22:30:36 +0800

    fix: product gallery page fixed

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CA_Proj.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace CA_Proj.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductImage { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductDownloadLink { get; set; }
+        public double ProductOverallRating { get; set; }
+        public string ProductKeywords { get; set; }
+        public int ProductQuantitySold { get; set; }
+
+    }
+}
